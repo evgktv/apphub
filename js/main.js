d1fc7038aade9f89fbed83646165f4076bbe24d9
evgktv <evgktv@gmail.com>
2018-11-06 01:50:25 +0300

    add scroll to anchors

--- js/main.js
+++ js/main.js
@@ -253,3 +253,21 @@ sliderBox.addEventListener('touchend', function(event) {
 			}
 		}
 }, false);
+
+
+// SCROLL FOR ANCHORS
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href')
+
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
